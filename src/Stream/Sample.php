10d9a099a5e2bd97112e069a908d01169c2a753f
Tamás Tamás <kovacs.tamas.hun@gmail.com>
2017-01-27 23:58:08 +0000

    Sample will use the same values as the base class.

--- src/Stream/Sample.php
+++ src/Stream/Sample.php
@@ -21,27 +21,4 @@ class Sample extends Stream
 	 * @var string
 	 */
 	protected $endpoint = '/1.1/statuses/sample.json';
-
-	/**
-	 * Parameters to use to filter the statuses.
-	 *
-	 * @var \Callisto\RequestParameter[]
-	 */
-	protected $requestParameters = [];
-
-	/**
-	 * Http method to use when connecting to the streaming API.
-	 *
-	 * @var string
-	 */
-	protected $httpMethod = 'GET';
-
-
-	/**
-	 * @return array
-	 */
-	protected function getParams() : array
-	{
-		return [];
-	}
 }
